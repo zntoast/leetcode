c8fdeb8307e370da2a2609176a10f1f8cd0a10c2
ZaiNeng <2878428463@qq.com>
2024-08-08 17:04:57 +0800

    // LC 3131. 找出与数组相加的整数 I

--- easy/easy.go
+++ easy/easy.go
@@ -1075,3 +1075,10 @@ func FindIntersectionValues(nums1 []int, nums2 []int) []int {
 	}
 	return []int{a, b}
 }
+
+// LC 3131. 找出与数组相加的整数 I
+func AddedInteger(nums1 []int, nums2 []int) int {
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+	return nums2[0] - nums1[0]
+}
