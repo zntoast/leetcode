c54ab2a0f41adfea364b811b6ac75635148740e2
ZaiNeng <2878428463@qq.com>
2024-10-15 10:53:54 +0800

    feat : 添加typescript语言的解题思路

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # leetcode
   leetcode的解题过程, 包含常见的数据结构，简单，中等，困难题解
+
+# 添加typeScript语言
+# npm i 
---
+++ easy/easy.ts
@@ -0,0 +1,14 @@
+/**
+ * @param {number} red
+ * @param {number} blue
+ * @return {number}
+ */
+var MaxHeightOfTriangle = function (red, blue) {
+  function dfs(red, blue, deep) {
+    if ((red < deep) && (blue < deep)) {
+      return 0
+    }
+  }
+};
+
+MaxHeightOfTriangle(2, 1)
