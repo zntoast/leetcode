6d2592beab9256f957244f0ab5c1fcf096dc1153
ZaiNeng <2878428463@qq.com>
2024-10-16 14:46:06 +0800

    // LC 3194. 最小元素和最大元素的最小平均值

--- easy/easy.ts
+++ easy/easy.ts
@@ -23,10 +23,21 @@ var MaxHeightOfTriangle = function (red, blue) {
   return Math.max(masHeight(red, blue), masHeight(blue, red))
 };
 
-console.log(MaxHeightOfTriangle(2, 4))
-console.log(MaxHeightOfTriangle(2, 1))
-console.log(MaxHeightOfTriangle(1, 1))
-console.log(MaxHeightOfTriangle(10, 1))
+// console.log(MaxHeightOfTriangle(2, 4))
+// console.log(MaxHeightOfTriangle(2, 1))
+// console.log(MaxHeightOfTriangle(1, 1))
+// console.log(MaxHeightOfTriangle(10, 1))
 
 
 
+// LC 3194. 最小元素和最大元素的最小平均值
+function MinimumAverage(nums: number[]): number {
+  nums.sort((a, b) => a - b)
+  const len = nums.length
+  let ans = nums[len - 1]
+  for (let i = 0; i < len / 2; i++) {
+    let temp = (nums[i] + nums[len - i - 1]) / 2
+    ans = Math.min(ans, temp)
+  }
+  return ans
+};
